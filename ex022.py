5a748e682ae0d44be3b2e5571293761c161df758
Renan Galego <107080173+RenanGalego@users.noreply.github.com>
2022-10-26 15:13:47 -0300

    Create ex022.py

---
+++ ex022.py
@@ -0,0 +1,10 @@
+""" Tratamento de erro divisão por 0 """
+
+def divisor(x,y):
+    try:
+        resultado = x/y
+        print(f'Resultado de {x}/{y} é {resultado}')
+    except ZeroDivisionError:
+        print("Não se faz divisão por zero!")
+
+divisor(3,0)
